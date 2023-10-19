07daef5a70d7e974b243ad30083e30d644dcac4a
Rickson de Oliveira Viana <rickson2013oliveira@gmail.com>
2023-10-19 16:11:21 -0300

    v0.6 - fix ButtonComponent.jsx

--- src/components/Button/ButtonComponent.jsx
+++ src/components/Button/ButtonComponent.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 
+import { Link } from "react-router-dom";
+
 import { ButtonStyle } from "./ButtonComponent.styled";
 
 const ButtonComponent = ({ buttonName, onClick }) => {
   return (
-    <ButtonStyle
-      whileHover={{
-        outline: "solid 5.5rem rgba(255, 255, 255, 0.1036)",
-      }}
-      initial={{ outline: "solid 0rem rgba(255, 255, 255, 0)" }}
-    >
-      {buttonName}
-    </ButtonStyle>
+    <Link to="/Destination">
+      <ButtonStyle
+        whileHover={{
+          outline: "solid 5.5rem rgba(255, 255, 255, 0.1036)",
+        }}
+        initial={{ outline: "solid 0rem rgba(255, 255, 255, 0)" }}
+      >
+        {buttonName}
+      </ButtonStyle>
+    </Link>
   );
 };
 
